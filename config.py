259361ded4bd46030fa2a1c61abf9debd5f3e6db
ansh224466 <143604025+ansh224466@users.noreply.github.com>
2024-04-14 19:27:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,8 +31,8 @@ PORT = os.environ.get("PORT", "8080")
 DB_URI = "mongodb+srv://rjkundra:Kayamkhani@cluster0.s6lvxxs.mongodb.net/?retryWrites=true&w=majority"
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "instantlinks.site")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "b90ab0b14abf1bf82a5d63b30c151ad38fdb9fcc")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "link2paisa.com")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "67cd1842583a6ff0d22faa67d97e98191f01ed49")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
 TUT_VID = os.environ.get("TUT_VID","https://t.me/how_bot_works/10")
