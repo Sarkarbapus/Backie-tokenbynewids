8f914cfc63dabdf44ba150f6482bc6893e1f8930
ansh224466 <143604025+ansh224466@users.noreply.github.com>
2024-04-14 19:28:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "link2paisa.com")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "67cd1842583a6ff0d22faa67d97e98191f01ed49")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
-TUT_VID = os.environ.get("TUT_VID","https://t.me/how_bot_works/10")
+TUT_VID = os.environ.get("TUT_VID","https://t.me/howtoopenchicken/6")
 
 
 #force sub channel id, if you want enable force sub
